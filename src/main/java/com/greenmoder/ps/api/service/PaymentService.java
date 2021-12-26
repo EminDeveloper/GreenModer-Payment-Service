9564b6afbfe6b464f2c365767484f73ad0f19fc1
Andrey <emin.f.mamedov@gmail.com>
2021-12-26 13:56:38 +0400

    Payment Service

---
+++ src/main/java/com/greenmoder/ps/api/service/PaymentService.java
@@ -0,0 +1,39 @@
+package com.greenmoder.ps.api.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.greenmoder.ps.api.entity.Payment;
+import com.greenmoder.ps.api.repository.PaymentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+import java.util.UUID;
+
+@Service
+public class PaymentService {
+    @Autowired
+    private PaymentRepository repository;
+
+    private Logger log = LoggerFactory.getLogger(PaymentService.class);
+
+    public Payment doPayment(Payment payment) throws JsonProcessingException {
+        payment.setPaymentStatus(paymentProcessing());
+        payment.setTransactionId(UUID.randomUUID().toString());
+        log.info("Payment Service request: {}", new ObjectMapper().writeValueAsString(payment));
+        return repository.save(payment);
+    }
+
+    public String paymentProcessing(){
+        return new Random().nextBoolean()?"success":"false";
+    }
+
+    public Payment findPaymentHistoryByOrderId(int orderId) throws JsonProcessingException {
+        Payment payment = repository.findByOrderId(orderId);
+        log.info("PaymentService findPaymentHistoryByOrderId: {}", new ObjectMapper().writeValueAsString(payment));
+        return payment;
+    }
+
+}
---
+++ src/test/java/com/greenmoder/ps/api/GreenPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenmoder.ps.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenPaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
